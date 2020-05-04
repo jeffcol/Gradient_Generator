455e3f442e3a1819a5fb19bd6bccdb169be3879a
Jefferson Villarreal <villa8ks@gmail.com>
2020-05-04 16:53:59 -0500

    Almost complete

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
+var clipboard = new ClipboardJS('.btn');
+
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let copyg = document.querySelector('.copyg');
 
 let whole_doc = document.querySelector('#gradient');
+
+let btn = document.querySelector('.btn');
 
 color1.addEventListener('input', coloring);
 color2.addEventListener('input', coloring);
@@ -14,5 +18,15 @@ function coloring(e) {
     let left = color2.value;
 
     whole_doc.style.background = "linear-gradient(to right," + right + "," + left +")";
+
+    let text1 = whole_doc.style.background;
+
+    copyg.textContent = text1;
+    btn.setAttribute("data-clipboard-text", text1);
+
+    alert(text1);
+    /*data-clipboard-text*/
+    
+    
 
 }
